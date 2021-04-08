64aec5086ceed5af7089d1330d9138110373cd9a
Panagiotis Kavadias <p.kavvadias@upnet.gr>
2021-04-09 02:05:47 +0300

    Add OMDB key to env

--- services/API/src/utils/environment.js
+++ services/API/src/utils/environment.js
@@ -3,5 +3,6 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../../../.env') })
 
 module.exports = {
     API_APP_PORT:process.env.API_APP_PORT,
-    JWT_SECRET:process.env.JWT_SECRET
+    JWT_SECRET:process.env.JWT_SECRET,
+    OMDB_KEY:process.env.OMDB_KEY
 }
