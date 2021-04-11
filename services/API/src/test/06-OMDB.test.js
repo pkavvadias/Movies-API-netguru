27d8d393a06d7a3afa2202bb6785e395d6fe022e
Panagiotis Kavadias <p.kavvadias@upnet.gr>
2021-04-11 19:52:43 +0300

    Remove unneeded comment

--- services/API/src/test/06-OMDB.test.js
+++ services/API/src/test/06-OMDB.test.js
@@ -12,7 +12,6 @@ describe('OMDB function test', function () {
 
     it('Should return movie object', async function () {
         stub.resolves(mocks.OMDBmock_correct);
-        //OMDB.movieInfo.restore();
         expect(await OMDB.movieInfo('Godfather')).to.deep.equal(mocks.godfather_object);
 
     });
